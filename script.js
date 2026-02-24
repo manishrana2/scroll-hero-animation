d896aa661122c267e6914c8ea091678e8e09ccf3
manishrana2 <manishrana82184@gmail.com>
2026-02-24 22:32:35 +0530

    fix gsap load issue

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", function () {
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -22,44 +22,7 @@ tl.to(".headline span",{
 },"-=0.3");
 
 
-let scrollTL = gsap.timeline({
-    scrollTrigger:{
-        trigger:".hero",
-        start:"top top",
-        end:"+=150%",
-        scrub:true,
-        pin:true
-    }
-});
-
-scrollTL.to(".headline",{scale:0.9,opacity:0.4,ease:"none"},0);
-scrollTL.to(".stats",{opacity:0,y:-40,ease:"none"},0);
-scrollTL.to(".main-image",{y:-200,scale:1.2,ease:"none"},0);
-
-});
-gsap.registerPlugin(ScrollTrigger);
-
-/* ===== LOAD ANIMATION ===== */
-
-let tl = gsap.timeline();
-
-tl.to(".headline span",{
-    y:0,
-    opacity:1,
-    duration:0.6,
-    stagger:0.05,
-    ease:"power3.out"
-})
-
-.to(".stat",{
-    opacity:1,
-    y:-10,
-    duration:0.6,
-    stagger:0.2
-},"-=0.3");
-
-
-/* ===== FINAL SCROLL ANIMATION ===== */
+/* ===== SCROLL ANIMATION ===== */
 
 let scrollTL = gsap.timeline({
     scrollTrigger:{
@@ -70,23 +33,23 @@ let scrollTL = gsap.timeline({
         pin:true
     }
 });
-
-/* Clean requirement animation */
 
 scrollTL.to(".headline",{
     scale:0.9,
-    opacity:0.4,
+    opacity:0.6,
     ease:"none"
-},0.3);
+},0.5);
 
 scrollTL.to(".stats",{
     opacity:0,
     y:-40,
     ease:"none"
-},0.3);
+},0.5);
 
 scrollTL.to(".main-image",{
-    y:-200,
+    y:-80,
     scale:1.2,
     ease:"none"
 },0);
+
+});
